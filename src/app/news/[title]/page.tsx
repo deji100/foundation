44bb33a42ade756bb35e9084b068db76814c507e
deji100 <adeyemideji9@gmail.com>
2025-04-11 13:02:41 +0100

    updated

--- src/app/news/[title]/page.tsx
+++ src/app/news/[title]/page.tsx
@@ -3,13 +3,13 @@ import MainPageNewsSection from "../../components/main-page/main-page-news-secti
 import NewsSection from "../../components/news/news-section";
 import { newsItems } from "../../components/data";
 
-// interface NewsPageProps {
-//   params: {
-//     title: string;
-//   };
-// }
+interface NewsPageProps {
+  params: {
+    title: string;
+  };
+}
 
-const News = async ({ params }: any) => {
+const News = async ({ params }: NewsPageProps) => {
   const decodedTitle = decodeURIComponent(params.title);
   const news = newsItems.find((item) => item.title === decodedTitle);
 
